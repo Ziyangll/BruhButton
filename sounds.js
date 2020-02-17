9805aa08cd5eb3f8dbd11a2dcfb16d3d8e9e9c19
Ziyang Li <zzl0106@auburn.edu>
2020-02-16 21:31:47 -0600

    bruh

--- sounds.js
+++ sounds.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll(".drum");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        const sound = new Audio("sounds/Bruh.mp3");
+        const sound = new Audio("sounds/" + this.innerHTML + ".mp3");
         sound.play().then(r => null);
     });
 }
